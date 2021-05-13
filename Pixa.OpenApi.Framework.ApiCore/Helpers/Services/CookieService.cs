c56be363036037b20805469ee437b8ab148981db
kamal1887 <kamal.gupta@bold.com>
2021-05-13 11:55:05 +0530

    cookie helper

---
+++ Pixa.OpenApi.Framework.ApiCore/Helpers/Services/CookieService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pixa.OpenApi.Framework.ApiCore.Helpers.Services
+{
+    public class CookieService : ICookieService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public CookieService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Delete(string key)
+        {
+            _httpContextAccessor.HttpContext.Response.Cookies.Delete(key);
+        }
+
+        public string Read(string key)
+        {
+            return _httpContextAccessor?.HttpContext?.Request?.Cookies[key];
+        }
+
+        public T Read<T>(string key)
+        {
+            T result = default;
+            string cookieValue = _httpContextAccessor?.HttpContext?.Request?.Cookies[key];
+            if (!string.IsNullOrEmpty(cookieValue))
+                result = JsonSerializer.Deserialize<T>(cookieValue);
+            return result;
+        }
+
+        public IDictionary<string, string> ReadAll()
+        {
+            IDictionary<string, string> _cookies = new Dictionary<string, string>();
+            if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Request != null
+                            && _httpContextAccessor.HttpContext.Request.Cookies != null)
+            {
+                foreach (var key in _httpContextAccessor.HttpContext.Request.Cookies.Keys)
+                {
+                    _cookies.Add(new KeyValuePair<string, string>(key, Read(key)));
+                }
+            }
+            return _cookies;
+        }
+
+        public void Write(string key, string value, string domain, int expiryDays, bool isSecure)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                Secure = isSecure,
+                SameSite = SameSiteMode.None
+            };
+            if (expiryDays > 0) cookieOptions.Expires = DateTime.Now.AddDays(expiryDays);
+            if (!string.IsNullOrEmpty(domain)) cookieOptions.Domain = "." + domain;
+            _httpContextAccessor.HttpContext.Response.Cookies.Append(key, value, cookieOptions);
+        }
+    }
+}
---
+++ Pixa.OpenApi.Framework.ApiCore/Helpers/Services/ICookieService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Pixa.OpenApi.Framework.ApiCore.Helpers.Services
+{
+    public interface ICookieService
+    {
+        /// <summary>
+        /// Add a new cookie and value 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="domain"></param>
+        /// <param name="expiryDays"></param>
+        /// <param name="isSecure"></param>
+        void Write(string key, string value, string domain, int expiryDays, bool isSecure);
+
+        /// <summary>
+        /// Get value of cookie
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        T Read<T>(string key);
+
+        /// <summary>
+        /// Get value of cookie
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        string Read(string key);
+
+
+        /// <summary>
+        /// Get list of all cookie
+        /// </summary>
+        /// <returns></returns>
+        IDictionary<string, string> ReadAll();
+
+        /// <summary>
+        /// Set an expired cookie
+        /// </summary>
+        /// <param name="key"></param>
+        void Delete(string key);
+    }
+}
